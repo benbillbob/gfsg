75fab335f6f976b27d6345d581eb180dc996556f
ben@benbillbob.com <ben@benbillbob.com>
2017-09-20 05:34:15 +0000

    #3 google analytics

--- www/mysite/code/GFSGMemberExtension.php
+++ www/mysite/code/GFSGMemberExtension.php
@@ -4,8 +4,39 @@ class GFSGMemberExtension extends DataExtension {
 	private static $db = array(
 		'TxnId' => 'Varchar(60)',
 		'MembershipExpiry' => 'Date',
-		'WithJournal' => 'Boolean'
+		'WithJournal' => 'Boolean',
+		'Parent2FirstName' => 'Varchar(255)',
+		'Parent2LastName' => 'Varchar(255)',
+		'HomeAddress' => 'Text',
+		'PostalAddress' => 'Text',
+		'Parent2Email' => 'Varchar(255)',
+		'HomePhone' => 'Varchar',
+		'Mobile' => 'Varchar',
+		'Mobile2' => 'Varchar',
+		'OccupationParent1' => 'Varchar',
+		'OccupationParent2' => 'Varchar',
+		'Child1Name' => 'Varchar(255)',
+		'Child1DOB' => 'Date',
+		'Child2Name' => 'Varchar(255)',
+		'Child2DOB' => 'Date',
+		'Child3Name' => 'Varchar(255)',
+		'Child3DOB' => 'Date',
+		'Child4Name' => 'Varchar(255)',
+		'Child4DOB' => 'Date',
+		'Child5Name' => 'Varchar(255)',
+		'Child5DOB' => 'Date'
 	);
+	
+	/*
+• Name – (with options for parent 1 and parent 2)
+• Home Address
+• Postal address (if different from above)
+• Email address (with options for parent 1 and parent 2)
+• Home Phone
+• Mobile/s
+• Occupations *Full and Professional memberships only
+• Names of children and children’s date of birth *Full memberships only
+	*/
 
   	private static $has_one = array(
 		'Membership' => 'Membership'
@@ -21,6 +52,10 @@ class GFSGMemberExtension extends DataExtension {
 	$fields->push($membershipField = DropdownField::create('MembershipID', 'Membership', Membership::get()->map('ID', 'Title')));
     $fields->push(new DateField('MembershipExpiry', 'MembershipExpiry'));
     $fields->push(new CheckboxField('WithJournal', 'WithJournal'));
+    $fields->push(new TextField('Parent2FirstName', 'Parent2FirstName'));
+    $fields->push(new TextField('Parent2LastName', 'Parent2LastName'));
+    $fields->push(new TextAreaField('HomeAddress', 'HomeAddress'));
+    $fields->push(new TextAreaField('PostalAddress', 'PostalAddress'));
 	
 	$membershipField->setHasEmptyDefault(true);
 	return $fields;
--- www/themes/simple2/templates/Page.ss
+++ www/themes/simple2/templates/Page.ss
@@ -27,6 +27,7 @@ Change it, enhance it and most importantly enjoy it!
 	<% require themedCSS('form') %>
 	<% require themedCSS('layout') %>
 	<link rel="shortcut icon" href="$ThemeDir/images/favicon.ico" />
+	$GoogleAnalytics
 </head>
 <body class="$ClassName<% if not $Menu(2) %> no-sidebar<% end_if %>" <% if $i18nScriptDirection %>dir="$i18nScriptDirection"<% end_if %>>
 <% include Header %>
